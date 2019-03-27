46b4cba93ca127357f385ca396a1fa01efb43aef
Cancain <eriksson.tomas@gmail.com>
2019-03-27 14:07:28 +0100

    19:37

--- src/print.rs
+++ src/print.rs
@@ -4,4 +4,26 @@ pub fn run() {
 
     //Basic formatting
     println!("{} is from {}", "Dowie", "Gävle");
+
+    //Positional arguments
+    println!(
+        "{0} is form {1} and {0} likes to {2}",
+        "Dowie", "Gävle", "code"
+    );
+
+    //Named arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "Dowie",
+        activity = "Overwatch"
+    );
+
+    //Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    //Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+
+    //Basic math
+    println!("10 + 10 = {}", 10 + 10);
 }
